9fa325e0248789ecac54a1397cc6b33cde133ccc
Madhan Raj <madhanraj242003@gmail.com>
2025-09-14 23:46:40 +0530

    Modified db.js

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,19 @@
 const { Pool } = require("pg");
+require("dotenv").config(); 
 
-const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "todo_db",
-  password: "Maddy24",
-  port: 5432,
-});
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false }, 
+      }
+    : {
+        user: "postgres",
+        host: "localhost",
+        database: "todo_db",
+        password: "Maddy24",
+        port: 5432,
+      }
+);
 
 module.exports = pool;
